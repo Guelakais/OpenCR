4f788c22a8b520469f4c4bd61be5732fd2671e8f
GuelaKais <koroyeldiores@gmail.com>
2023-08-14 23:52:02 +0200

    Baby steps into micro ros

---
+++ arduino/opencr_arduino/opencr/libraries/microRos/micro.ino
@@ -0,0 +1,8 @@
+#include <TurtleBot3_ROS2.h>
+void setup(){
+  TurtleBot3Core::begin("eurobotMAMUTtwin");
+}
+void loop(){
+  // Run TurtleBot3 core for communicating with ROS2 node, sensing several sensors and controlling actuators.
+  TurtleBot3Core::run();
+}
